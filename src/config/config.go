9fb83f6c4bb4fb9166ff6a57e15494475c13895a
Sebastian Weddmark Olsson <visnae@gmail.com>
2021-02-18 17:40:52 +0100

    Move read config to config package

--- src/config/config.go
+++ src/config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
 // Configurations exported
 type Configurations struct {
 	Server       ServerConfigurations
@@ -18,4 +25,31 @@ type DatabaseConfigurations struct {
 	DBName     string
 	DBUser     string
 	DBPassword string
+}
+
+func ReadConfig(configuration *Configurations) {
+	// Set the path to look for the configurations file
+	viper.AddConfigPath("./config")
+
+	// Enable VIPER to read Environment Variables
+	viper.AutomaticEnv()
+
+	// Config file to read
+	viper.SetConfigType("yaml")
+	viper.SetDefault("CONFIG_FILE", "local")
+	file := viper.GetString("CONFIG_FILE")
+	log.Print("Reading config file: ", file)
+	viper.SetConfigName(file)
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	// Set undefined variables
+	viper.SetDefault("database.dbname", "test_db")
+
+	err := viper.Unmarshal(configuration)
+	if err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+	}
 }
--- src/sidan-backend.go
+++ src/sidan-backend.go
@@ -11,37 +11,10 @@ import (
 	r "github.com/sebastiw/sidan-backend/src/router"
 )
 
-func read_config(configuration *c.Configurations) {
-	// Set the path to look for the configurations file
-	viper.AddConfigPath("./config")
-
-	// Enable VIPER to read Environment Variables
-	viper.AutomaticEnv()
-
-	// Config file to read
-	viper.SetConfigType("yaml")
-	viper.SetDefault("CONFIG_FILE", "local")
-	file := viper.GetString("CONFIG_FILE")
-	log.Print("Reading config file: ", file)
-	viper.SetConfigName(file)
-
-	if err := viper.ReadInConfig(); err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
-	}
-
-	// Set undefined variables
-	viper.SetDefault("database.dbname", "test_db")
-
-	err := viper.Unmarshal(configuration)
-	if err != nil {
-		fmt.Printf("Unable to decode into struct, %v", err)
-	}
-}
-
 func main() {
 	var configuration c.Configurations
 
-	read_config(&configuration)
+	c.ReadConfig(&configuration)
 
 	// sql.connect(connect_config)
 
