277c5981bb7c81468ac9ff5651f9616f412647aa
Sebastian Weddmark Olsson <visnae@gmail.com>
2021-02-28 22:06:34 +0100

    cleanup member routing and logging

--- src/router/router.go
+++ src/router/router.go
@@ -36,13 +36,59 @@ func tracing(nextRequestID func() string) func(http.Handler) http.Handler {
 	}
 }
 
-func logging(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			log.Println(get_request_id(r), r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
-		}()
-		h.ServeHTTP(w, r)
-	})
+
+type statusWriter struct {
+	http.ResponseWriter
+	status int
+	length int
+}
+
+func (w *statusWriter) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
+
+func (w *statusWriter) Write(b []byte) (int, error) {
+	if w.status == 0 {
+		w.status = 200
+	}
+	n, err := w.ResponseWriter.Write(b)
+	w.length += n
+	return n, err
+}
+
+type LogEntry struct {
+	RequestId string
+	Host string
+	RemoteAddr string
+	Method string
+	RequestURI string
+	Proto string
+	Status int
+	ContentLen int
+	UserAgent string
+	Duration time.Duration
+}
+
+func LogHTTP(handler http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		sw := statusWriter{ResponseWriter: w}
+		handler.ServeHTTP(&sw, r)
+		duration := time.Now().Sub(start)
+		log.Println(LogEntry{
+			RequestId:  get_request_id(r),
+			Host:       r.Host,
+			RemoteAddr: r.RemoteAddr,
+			Method:     r.Method,
+			RequestURI: r.RequestURI,
+			Proto:      r.Proto,
+			Status:     sw.status,
+			ContentLen: sw.length,
+			UserAgent:  r.Header.Get("User-Agent"),
+			Duration:   duration,
+		})
+	}
 }
 
 func next_request_id() string {
@@ -68,39 +114,45 @@ func Mux(db *sql.DB) http.Handler {
 	// r.HandleFunc("/mail", defaultHandler)
 	// r.HandleFunc("/notify", defaultHandler)
 
-	r.HandleFunc("/db/member", createMemberHandler).Methods("PUT")
-	r.HandleFunc("/db/member/{id:[0-9]+}", readMemberHandler).Methods("GET")
-	r.HandleFunc("/db/member/{id:[0-9]+}", updateMemberHandler).Methods("POST")
-	r.HandleFunc("/db/member/{id:[0-9]+}", deleteMemberHandler).Methods("DELETE")
-	r.HandleFunc("/db/members", readAllMemberHandler).Methods("GET")
+	mh := MemberHandler{db: db}
+
+	r.HandleFunc("/db/member", mh.createMemberHandler).Methods("PUT")
+	r.HandleFunc("/db/member/{id:[0-9]+}", mh.readMemberHandler).Methods("GET")
+	r.HandleFunc("/db/member/{id:[0-9]+}", mh.updateMemberHandler).Methods("POST")
+	r.HandleFunc("/db/member/{id:[0-9]+}", mh.deleteMemberHandler).Methods("DELETE")
+	r.HandleFunc("/db/members", mh.readAllMemberHandler).Methods("GET")
 
 	// r.HandleFunc("/db", defaultHandler)
 
-	return tracing(next_request_id)(logging(r))
+	return tracing(next_request_id)(LogHTTP(r))
 }
 
-func createMemberHandler(w http.ResponseWriter, r *http.Request) {
+type MemberHandler struct {
+	db *sql.DB
+}
+
+func (mh MemberHandler) createMemberHandler(w http.ResponseWriter, r *http.Request) {
 	var m model.Member
 	_ = json.NewDecoder(r.Body).Decode(&m)
 
 	log.Println(get_request_id(r), m.Fmt())
-	member := d.Create(db, m)
+	member := d.Create(mh.db, m)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(member)
 }
 
-func readMemberHandler(w http.ResponseWriter, r *http.Request) {
+func (mh MemberHandler) readMemberHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
 
-	member := d.Read(db, id)
+	member := d.Read(mh.db, id)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(member)
 }
 
-func updateMemberHandler(w http.ResponseWriter, r *http.Request) {
+func (mh MemberHandler) updateMemberHandler(w http.ResponseWriter, r *http.Request) {
 	var m model.Member
 	_ = json.NewDecoder(r.Body).Decode(&m)
 
@@ -109,15 +161,31 @@ func updateMemberHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Println(get_request_id(r), m.Fmt())
 	m.Id = int64(id)
-	member := d.Update(db, m)
+	member := d.Update(mh.db, m)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(member)
 }
 
-func readAllMemberHandler(w http.ResponseWriter, r *http.Request) {
-	members := d.ReadAll(db)
+func (mh MemberHandler) deleteMemberHandler(w http.ResponseWriter, r *http.Request) {
+	var m model.Member
+	_ = json.NewDecoder(r.Body).Decode(&m)
+
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	log.Println(get_request_id(r), m.Fmt())
+	m.Id = int64(id)
+	member := d.Delete(mh.db, m)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(member)
+}
+
+func (mh MemberHandler) readAllMemberHandler(w http.ResponseWriter, r *http.Request) {
+	members := d.ReadAll(mh.db)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(members)
 }
+
