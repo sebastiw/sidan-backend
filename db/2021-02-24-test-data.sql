25706d886f448cded3859f4e0a26580da436588b
Sebastian Weddmark Olsson <visnae@gmail.com>
2021-03-14 19:34:55 +0100

    more test data

--- db/2021-02-24-test-data.sql
+++ db/2021-02-24-test-data.sql
@@ -1,4 +1,5 @@
 /* https://www.fakenamegenerator.com/advanced.php?t=country&n%5B%5D=sw&c%5B%5D=sw&gen=100&age-min=19&age-max=50 */
+/* https://www.ipvoid.com/random-ip/ */
 
 INSERT INTO `cl2007_members` VALUES
   (295,8,'Elias Lundgren','EliasLundgren@armyspy.com','EliasLundgren@armyspy.com','013-4661218','Degerfors','','Wasellift','Design printing machine operator','<a href="http://localhost/">here</a>','    02cd8fa7-ebde-44cf-9a1a-abbb409d2204',1,'thePhahs9ri','',''),
@@ -11,3 +12,59 @@ INSERT INTO `cl2007_members` VALUES
   (317,30,'Frank Lundgren','FrankLundgren@jourrapide.com','RhodeIslandBargains.se','006573690701','Morvall Färilavägen 58, 82044 TALLÅSEN','https://www.google.com/maps?hl=en&q=61.900924,+16.059333','Cend1999','Transportation attendant','<a href="http://localhost/">here</a> <a href="http://localhost/">here</a> <a href="http://localhost/">here</a>','a6398c79-1832-42b1-adac-b2f8f4b058f5',1,'eej4ooPoo4ie','',''),
   (288,47,'Kenan Persson','KenanPersson@rhyta.com','','0325-4545513','1Z 734 094 19 9309 053 5','','Coluseld','Clerical supervisor','<a href="http://localhost/">here</a> <a href="http://localhost/">here</a>',NULL,1,'EDoochukai6','',''),
   (296,9,'Melvin Sjögren','','','','','','Entriank','Color printer operator','',NULL,1,'','','');
+
+
+INSERT INTO `cl2007_prospects` VALUES
+  (256,'P',917,'Holger','','0499-2266502','Pisces/Housekeeper 17'),
+  (259,'P',422,'Arthur Hansson','ArthurHansson@rhyta.com','0240-6232243','Electrical drafter'),
+  (241,'S',7,'Yasin Lundström','YasinLundstrom@teleworm.us','0612-6931093','Business office assistant/Appliance repairer'),
+  (265,'S',995,'Didrik Olofsson','DidrikOlofsson@dayrep.com','0435-9256394','Publicity expert'),
+  (258,'S',64,'Loke Hermansson','LokeHermansson@teleworm.us','0418-4957631','Epidemiologist'),
+  (239,'S',111,'Alve','AlveHermansson@dayrep.com','0480-3487979','Dance captain');
+
+INSERT INTO `cl2015_arrsidan` VALUES
+  (18,'','2001-07-22 12:00','Prag','#10','#10,#3,#8','','','','false'),
+  (19,'','2001-08-10 22:00','Supa, Supa, Supa','#15','#15,GuiGui,#27,#18','','','','false'),
+  (45,'','2001-11-24 23:59','Kallerummen _efterfest med Chalmersbaletten_','#18','#18,#27,#25,#24,#26','','','','false'),
+  (47,'','2001-11-29 20:00','Fetpizza på Rotary _ röj','#27','#27,#25,PB,#18,#8,#15,#3,#22,#4','','','','false'),
+  (2062,'ANDRA LÅNGGATAN','2020-06-23 17:53','','','#8,#95,#71','','',NULL,'false'),
+  (2063,'Flakoff','2020-06-27 13:37','Bakom Fokus','#93','#93,S38,S99,S1347,#96','','','','false'),
+  (2079,'Onsdagsråtta','2020-10-21 20:00','','#83','#83,#94','','',NULL,'false'),
+  (2080,'Måndagsbärs','2024-11-02 18:00','Sjuan','#8, #82','#8, #82','','',NULL,'false');
+
+INSERT INTO `cl_news` VALUES
+  (1,'Loser eldade upp halv mille','En man i Jönköping misstänks för att ha eldat upp närmare en halv  miljon i kontanter i samband med en skilsmässa och personlig konkurs.\r\n  Det är konkursförvaltaren som polisanmält mannen, uppger Sveriges\r\n  Radio Jönköping. \r\n\r\n  Misstanken gäller grov oredlighet mot borgenär. Enligt konkurs förvaltaren eldade mannen upp pengarna bland annat för att hans före  detta hustru inte skulle få del av dem. \r\n  Pengabrasan fick också till följd att mannen försattes i personlig  konkurs. (TT)','2002-02-18','20:10:12'),
+  (3,'Strålning hotar storleken','När Statstjänstemannaförbundet första gången krävde strålningsfria bildskärmar var det ingen som lyssnade. Men när en expert på Arbetarsskyddstyrelsen berättat om ett försök, där det visat sig att elektromagnetisk strålning orsakar förkrympta könsorgan, då hände det saker. Och kravet togs på allvar. Allt enligt Peter Magnusson på Statstjänstemannaförbundet.\r\n(Källa: Dagens IT)\r\n\r\n#27','2002-02-06','17:11:15'),
+  (26,'Strecklista sportstugearret','nr;50cl;33cl;sprit\r\n#1 6 1 0\r\n#5 3 3 0\r\n#6 5 3 3\r\n#69 5 2 2\r\n#13 8 2 3\r\nP2 3 2 0\r\nP4 5 3 0\r\nP7 5 3 2','2002-05-02','23:56:53'),
+  (222,'Test av L-cystein mot bakfylla','Jag har under senaste åren kunnat utröna att efter sex enheter mår jag piss en halv dag och efter tio enheter kan jag glömma att ens fundera på mat dagen efter. Därför blev jag väldigt nyfiken när följande postades på sidan\r\n\r\nÄntligen! https://interestingengineering.com/hangover-cure-reduces-headaches-anxiety-for-drunk-finnish-subjects\r\n#62 @ Karta | 2020-08-21, 10:47:10 (fre) \r\n\r\nDet visade sig att jag kunde beställa medlet på nätet och hade en kräftskiva inplanerad helgen efter. Time for science!\r\n\r\nHypotes:\r\nGenom att hålla en hög nivå av L-cystein i kroppen under supande blir baksmällan lättare.\r\n\r\nMetod:\r\nTre försökspersoner med normalt till lätt överviktigt bmi, två män j j en kvinna, tog 1 kapsel med 500 mg L-cystein innan supande och en 1 kapsel vid kulmen av supande. Normal kräftskivekonsumtion av alkohol. Försökspersonernas mående rapporterades anekdotiskt dagen efter.\r\n\r\nResultat:\r\nTvå av försökspersonerna rapporterade att de mådde fina fisken, bättre än de hade förväntat sig. Den tredje som enligt egen utsago gick ut hårdare än väntat var däremot så trasig som han förväntade sig. Till protokollet ska även tilläggas att han gick upp mitt i natten för att spy och att han  tog i lite för mycket för att ett litet piller skulle räddas mig .\r\n\r\nDiskussion:\r\nDå författaren deltog i studien och är väldigt bekant med förväntad nivå av bakfylla efter olika grader av alkoholintag kan han säga att han kände sig oförtjänt pigg, nästan som att han hade fuskat. Författarens sambo konstaterade att det inte fanns några tvivel om att dennes berusningsnivå kvällen innan var av en nivå som tidigare föranlett fosterställning, snabbantning och ångestdusch.\r\n\r\nSlutsats:\r\nUnderlaget är för litet för att dra några slutsatser, men den här preliminära studien tyder på att det finns fog att fortsätta utvärdera metoden.','2020-08-30','12:16:39');
+
+INSERT INTO `cl2004_poll` VALUES
+  (683,'På vilken hand sitter ingenjörsringen?','vänster','höger','2007-12-10','19:39:09','127.0.226.28','127-0-226-28.besome.co.uk'),
+  (739,'I vilken rikitning kör man i en cirkulationsplats?','Motsols','I båda beroende på var du kommer ifrån','2008-05-13','23:56:43','192.168.194.42','okokoakkk.gsgnet.com'),
+  (868,'Tiden..','rusar förbi!','krälar i sirap!','2009-01-19','17:20:45','13.255.80.200','c13-255-80-200.bredband.komhem.se'),
+  (1058,'Boka bastun?','Ja, fulla, fattiga och feta','Nej','2012-09-13','22:00:52','13.255.80.200','c13-255-80-200.bredband.komhem.se'),
+  (1283,'Vad ogillar ni med torsdagsarret?','Öl','Torsdag','2020-09-15','13:23:23','10.0.3.222','host.gsgnet.com');
+
+INSERT INTO `cl2004_poll_votes` VALUES
+  (1284,'80-68-91-178-no507.tbcn.felia.com',1),
+  (1284,'lusburk.vry.sgsnet.se',1),
+  (1284,'15-175-85-1.cust.bb.com',1),
+  (1284,'130-64-241-148.tukw.quest.arb',0),
+  (1284,'customer-166-56-132-163.stolny.net',0),
+  (1284,'c232-39-138-147.bredband.komhem.se',0),
+  (1284,'h-62-50-156-65.NA.cust.s-bahn.de',1),
+  (1284,'57-236-142-10-no514.tbcn.felia.com',1),
+  (1284,'30.12.20.232',0),
+  (1284,'cust-155-126-240-200.bosbonet.se',0),
+  (1284,'#13',0),
+  (1284,'53.179.232.167',0),
+  (1284,'h-149-73-234-248.NA.cust.s-bahn.de',1),
+  (1284,'231.80.81.238',1),
+  (1284,'c-bd8972d5.2340-10763f53.bbcust.phonenoir.se',1),
+  (1284,'#69',0),
+  (1284,'#1',0),
+  (1284,'h-144-120.A6168993.priv.s-bahn.de',0),
+  (1284,'114-190-3-216-no232.tbcn.felia.com',1),
+  (1284,'ip207.ip-39-182-73.eu',1),
+  (1284,'h-166-95-101-129.NA.cust.s-bahn.de',1),
+  (1284,'50-121-6-145-no24.tbcn.felia.com',1);
