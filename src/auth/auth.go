b96158fe35f4d56458159622d315ae9b0bcb06db
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2021-03-21 15:46:54 +0100

    add working login implementation

--- src/auth/auth.go
+++ src/auth/auth.go
@@ -1,10 +1,119 @@
 package auth
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"github.com/gorilla/securecookie"
+	"log"
+
+	"net/http/httputil"
 )
+
+const loginPage = `
+<h1>Login</h1>
+<form method="post" action="/auth">
+    <label for="name">User name</label>
+    <input type="text" id="name" name="name">
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password">
+    <button type="submit">Login</button>
+</form>
+`
+
+const homePage = `
+<h1>Internal</h1>
+<hr>
+<small>User: %s</small>
+<form method="post" action="/logout">
+    <button type="submit">Logout</button>
+</form>
+`
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+var cookieHandler = securecookie.New(
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
+
+func setSession(userName string, response http.ResponseWriter) {
+	value := map[string]string{
+		"name": userName,
+	}
+	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: encoded,
+			Path:  "/",
+		}
+		http.SetCookie(response, cookie)
+	}
+}
+
+func getUserName(request *http.Request) (userName string) {
+	if cookie, err := request.Cookie("session"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+			userName = cookieValue["name"]
+		}
+	}
+	return userName
+}
+
+func clearSession(response http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(response, cookie)
+}
+
+// Debug
+func LoginPageHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, loginPage)
+}
+
+// Debug
+func printRequest(req *http.Request){
+	// Save a copy of this request for debugging.
+	requestDump, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Println(string(requestDump))
+}
+
+func LoginHandler(response http.ResponseWriter, request *http.Request) {
+	name := request.FormValue("name")
+	pass := request.FormValue("password")
+	redirectTarget := "/"
+
+	log.Printf("Request:")
+	printRequest(request)
+	log.Printf("Name: %s Pass:%s",name, pass)
+	if name != "" && pass != "" {
+		// .. check credentials ..
+		setSession(name, response)
+		redirectTarget = "/home"
+	}
+	http.Redirect(response, request, redirectTarget, 302)
+}
+
+//Debug
+func LogoutHandler(response http.ResponseWriter, request *http.Request) {
+	clearSession(response)
+	http.Redirect(response, request, "/login", 302)
+}
+
+func HomePageHandler(response http.ResponseWriter, request *http.Request) {
+	userName := getUserName(request)
+	if userName != "" {
+		fmt.Fprintf(response, homePage, userName)
+		// fmt.Fprintf(response, internalPage, userName)
+	} else {
+		http.Redirect(response, request, "/login", 302)
+	}
 }
