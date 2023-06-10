366f7783ed1a844c669789bbf736424a0edc5978
Sebastian Weddmark Olsson <visnae@gmail.com>
2023-06-10 22:07:40 +0200

    Exchange Authorization for Access

--- src/router/auth.go
+++ src/router/auth.go
@@ -29,14 +29,10 @@ type OAuth2AccessToken struct {
 	TokenType   string `json:"token_type"`
 }
 
-func (oh OAuth2Handler) oauth2RedirectHandler(w http.ResponseWriter, r *http.Request) {
-	var conf *oauth2.Config
-
-	log.Println(getRequestId(r), oh)
-
+func (oh OAuth2Handler) oauth2Config() *oauth2.Config {
 	switch oh.Provider {
 	case "google":
-		conf = &oauth2.Config{
+		return &oauth2.Config{
 			ClientID:     oh.ClientID,
 			ClientSecret: oh.ClientSecret,
 			RedirectURL:  oh.RedirectURL,
@@ -44,7 +40,7 @@ func (oh OAuth2Handler) oauth2RedirectHandler(w http.ResponseWriter, r *http.Req
 			Endpoint:     google.Endpoint,
 		}
 	case "github":
-		conf = &oauth2.Config{
+		return &oauth2.Config{
 			ClientID:     oh.ClientID,
 			ClientSecret: oh.ClientSecret,
 			RedirectURL:  oh.RedirectURL,
@@ -54,6 +50,13 @@ func (oh OAuth2Handler) oauth2RedirectHandler(w http.ResponseWriter, r *http.Req
 	default:
 		panic(fmt.Errorf("provider not supported %s", oh.Provider))
 	}
+}
+
+
+func (oh OAuth2Handler) oauth2RedirectHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(getRequestId(r), oh)
+
+	conf := oh.oauth2Config()
 
 	// Generate the URL to redirect the user to for authentication
 	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
@@ -63,11 +66,16 @@ func (oh OAuth2Handler) oauth2RedirectHandler(w http.ResponseWriter, r *http.Req
 }
 
 func (oh OAuth2Handler) oauth2AuthCallbackHandler(w http.ResponseWriter, r *http.Request) {
-	// var conf *oauth2.Config
-
+	conf := oh.oauth2Config()
 	vars := mux.Vars(r)
 
 	e := OAuth2AuthToken{Code: vars["code"], State: vars["state"]}
 
-	log.Println(getRequestId(r), e)
+	token, err := conf.Exchange(oauth2.NoContext, e.Code)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
+
+	log.Println(getRequestId(r), token)
 }
