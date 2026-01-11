9ff88a77db91aec91b918e396bb4a6aa8490429e
gabrielsson <max.gabrielsson@gmail.com>
2026-01-11 22:22:54 +0100

    Fixing like columns time field and updated swagger with bearer auth security globally
    Signed-off-by: gabrielsson <max.gabrielsson@gmail.com>

--- src/models/like.go
+++ src/models/like.go
@@ -1,16 +1,12 @@
 package models
 
-import (
-	"time"
-)
-
 // Like represents a like on an entry
 type Like struct {
-	Date time.Time `gorm:"column:date;type:date" json:"date"`
-	Time time.Time `gorm:"column:time;type:time" json:"time"`
-	Id   int64     `gorm:"column:id" json:"id"`  // References cl2003_msgs.id
-	Sig  string    `gorm:"column:sig" json:"sig"`
-	Host string    `gorm:"column:host" json:"host"`
+	Date string `gorm:"column:date" json:"date"`
+	Time string `gorm:"column:time" json:"time"`
+	Id   int64  `gorm:"column:id" json:"id"`  // References cl2003_msgs.id
+	Sig  string `gorm:"column:sig" json:"sig"`
+	Host string `gorm:"column:host" json:"host"`
 }
 
 func (Like) TableName() string {
